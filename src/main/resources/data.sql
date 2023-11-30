53fa295cdbd3a6b7158d4232f5297d19fd2e10c2
smihman <smihman@gmail.com>
2023-11-30 11:05:59 +0100

    Add sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO item (id, name) VALUES (1, 'USA');
+INSERT INTO item (id, name) VALUES (2, 'France');
+INSERT INTO item (id, name) VALUES (3, 'Brazil');
+INSERT INTO item (id, name) VALUES (4, 'Italy');
+INSERT INTO item (id, name) VALUES (5, 'Canada');
