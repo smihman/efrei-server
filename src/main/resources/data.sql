ca6f3ffdc2dc5584c9438ec801812f76030a2f7c
smihman <smihman@gmail.com>
2023-11-30 11:37:16 +0100

    Add docker

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+DELETE FROM item;
+INSERT INTO item (id, name, price) VALUES (1, 'Shoes', 60.00);
+INSERT INTO item (id, name, price) VALUES (2, 'T-Shirt', 20.00);
+INSERT INTO item (id, name, price) VALUES (3, 'Jeans', 40.00);
+INSERT INTO item (id, name, price) VALUES (4, 'Sweater', 35.00);
+INSERT INTO item (id, name, price) VALUES (5, 'Hat', 15.00);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS item (
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
+    price DECIMAL(10, 2)
+);
