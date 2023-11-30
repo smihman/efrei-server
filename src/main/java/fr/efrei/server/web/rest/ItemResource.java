1423f866c9da40bae134820066be4e7c7cc0ceae
smihman <smihman@gmail.com>
2023-11-30 11:01:50 +0100

    add rest

--- src/main/java/fr/efrei/server/web/rest/ItemResource.java
+++ src/main/java/fr/efrei/server/web/rest/ItemResource.java
@@ -1,4 +1,4 @@
-package main.java.fr.efrei.web.rest;
+package fr.efrei.server.web.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
